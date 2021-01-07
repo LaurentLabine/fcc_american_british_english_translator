5360311f8a2bb94dde1981ad14c532f043d45111
Laurent Labine <laurentlabine@gmail.com>
2021-01-08 00:30:03 +0100

    American to british part mostly done
    Includes Capitalization on replaced words.
    
    Still need to implement emphasis and response.

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,87 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+//https://stackoverflow.com/questions/17264639/replace-text-but-keep-case
+
+    matchCase(text, pattern) {
+        var result = '';
+    
+        for(var i = 0; i < text.length; i++) {
+            var c = text.charAt(i);
+            var p = pattern.charCodeAt(i);
+    
+            if(p >= 65 && p < 65 + 26)
+                result += c.toUpperCase();
+            else
+                result += c.toLowerCase();
+        }
+        return result;
+    }
+
+    translate(text, locale){
+
+        var str = text
+        var regex
+
+        if(text == "")
+            return {error: "empty string"}
+
+        if(locale === "american-to-british"){
+            Object.keys(americanOnly).forEach(word =>{
+                console.log(word)
+                regex = new RegExp("\\b" + word + "\\b","gi")
+                
+                if(str.match(regex) != null)
+                    console.log("Match : " + str.match(regex))
+                
+                    if(regex.test(text))
+                    str = str.replace(regex, this.matchCase(americanOnly[word],str.match(regex).toString()))
+            })
+
+            Object.keys(americanToBritishTitles).forEach(word =>{
+                regex = new RegExp("\\b" + word + "\\b","gi")
+
+                if(str.match(regex) != null)
+                console.log("Match : " + str.match(regex))
+
+                if(regex.test(text))
+                    str = str.replace(regex, this.matchCase(americanToBritishTitles[word],  str.match(regex).toString()))
+            })
+
+            Object.keys(americanToBritishSpelling).forEach(word =>{
+                regex = new RegExp("\\b" + word + "\\b","gi")
+
+                if(str.match(regex) != null)
+                console.log("Match : " + str.match(regex))
+
+                if(regex.test(text))
+                    str = str.replace(regex, this.matchCase(americanToBritishSpelling[word],  str.match(regex).toString()))
+            })
+        }else if(locale === "british-to-american"){
+            Object.keys(britishOnly).forEach(word =>{
+                console.log(word)
+                regex = new RegExp("\\b" + word + "\\b","gi")
+                if(regex.test(text))
+                    str = str.replace(regex, britishOnly[word])
+                    
+            })
+
+            Object.keys(americanToBritishTitles).forEach(word =>{
+                regex = new RegExp("\\b" + americanToBritishTitles[word] + "\\b","gi")
+                if(regex.test(text))
+                    str = str.replace(regex, word)
+            })
+
+            Object.keys(americanToBritishSpelling).forEach(word =>{
+                regex = new RegExp("\\b" + americanToBritishSpelling[word] + "\\b","gi")
+                if(regex.test(text))
+                    str = str.replace(regex, word)
+            })
+        }
+
+    return {text: text, translation: str}   
+    }
+
 }
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,4 +5,44 @@ const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
 
+    suite('Translate to british English', function() {
+    
+        test('Mangoes are my favorite fruit.', function(done) {
+          let text = 'Mangoes are my favorite fruit.';
+          let locale = 'american-to-british'
+          assert.equal(Translator.translate(text,locale),{"text":"Mangoes are my favorite fruit.","translation":"Mangoes are my <span class=\"highlight\">favourite</span> fruit."});
+          done();
+        });
+        
+        test('Decimal Input', function(done) {
+          let input = '1.5L';
+          assert.equal(convertHandler.getNum(input),1.5);
+          done();
+        });
+        
+        test('Fractional Input', function(done) {
+          let input = '2/5L';
+          assert.equal(convertHandler.getNum(input),0.4);
+          done();
+        });
+        
+        test('Fractional Input w/ Decimal', function(done) {
+          let input = '1.1/2L';
+          assert.equal(convertHandler.getNum(input),0.55);
+          done();
+        });
+        
+        test('Invalid Input (double fraction)', function(done) {
+          let input = '1//2kg';
+          assert.equal(convertHandler.getNum(input),'invalid number');
+          done();
+        });
+        
+        test('No Numerical Input', function(done) {
+          let input = 'L';
+          assert.equal(convertHandler.getNum(input),1);
+          assert.equal(convertHandler.getUnit(input),'L')
+          done();
+        }); 
+
 });
